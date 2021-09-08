56d4154b2c2f7fbd6dea24b75ace2bb16f57bea7
ukdong <lklone20005@gmail.com>
2021-09-09 02:55:33 +0900

    vue-loader 사용 위해 webpack rules 추가 && vue-template-compiler 설치

--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,3 +1,4 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path')
 
 module.exports = {
@@ -6,10 +7,13 @@ module.exports = {
     },
     module: {
         rules: [{
-
+            test: /\.vue$/,
+            loader: 'vue-loader',
         }],
     },
-    plugins: [],
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
     output: {
         filename: '[name].js',
         path: path.join(__dirname, 'dist'),
