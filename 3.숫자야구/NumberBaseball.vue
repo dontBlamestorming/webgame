1e62bc50aebe6c02fea3092a91fbe78ac26b2d0f
ukdong <lklone20005@gmail.com>
2021-09-09 00:20:21 +0900

    add global component && index.html code

---
+++ 3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form @submit="onSubmitForm">
+      <input type="number" maxlength="4" ref="answer" v-model="value">
+      <button type="submit">입력</button>
+    </form>
+    <div>시도 : {{}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+      result: ''
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style></style>
