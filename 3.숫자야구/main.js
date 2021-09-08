e5052b79ac4462aa21d56028e5747a1f5fb5aad2
ukdong <lklone20005@gmail.com>
2021-09-09 00:19:19 +0900

    make vue instance

---
+++ 3.숫자야구/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+new Vue().$mount('#root')
+
+/*
+    create vue instance same like under code
+    <script>
+        const app = new Vue({
+            el: '#root',
+        })
+    </script>
+ */
