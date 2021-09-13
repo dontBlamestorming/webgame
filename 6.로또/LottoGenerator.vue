c676252d72ef1849d772b44749b58389bf946fdb
ukdong <lklone20005@gmail.com>
2021-09-14 01:32:13 +0900

    로또 번호 구현 코드 변경 및 각각의 ball 컴포넌트화

--- 6.로또/LottoGenerator.vue
+++ 6.로또/LottoGenerator.vue
@@ -1,41 +1,58 @@
 <template>
   <div>
     <div>당첨 숫자</div>
-    <div class="ball" v-for="(number, index) in numbers" :key="index">{{ number }}</div>
     <div id="결과창">
-      <lotto-ball v-for="(number, index) in numbers" :key="index">{{ number }}</lotto-ball>
+      <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball"></lotto-ball>
     </div>
     <div>보너스!</div>
-    <lotto-ball v-if="bonusNumber"></lotto-ball>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
     <button v-if="redo">한 번 더!</button>
   </div>
 </template>
 
 <script>
-let interval = null
+import LottoBall from "./LottoBall";
+
+function getWinNumbers() {
+  const shuffle = []
+  const candidate = Array(45).fill().map((value, index) => index + 1)
+
+  while (candidate.length > 0) {
+    // 1 ~ 45까지 임의의 수를 index로 활용하여 candidate에서 slice 이후 shuffle에 push
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length),1)[0])
+  }
+
+  const bonusNumber = shuffle[shuffle.length - 1]
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c) // ascending sort
+
+  return [...winNumbers, bonusNumber]
+}
 
 export default {
+  components: {
+    LottoBall,
+  },
   data() {
     return {
+      winNumbers: getWinNumbers(),
+      winBalls: [],
       redo: false,
-      bonusNumber: false,
-      numbers: [],
+      bonus: false,
     }
   },
   methods: {
-    genNumbers() {
-      const randomNumber = Math.floor(Math.random() * 45) + 1
-      this.numbers.push(randomNumber)
-    }
   },
   mounted() {
-    interval = setInterval(() => {
-      if (this.numbers.length < 6) {
-        this.genNumbers()
-      } else {
-        clearInterval(interval)
-      }
-    }, 1500)
+    for (let i = 0; i < this.winNumbers.length - 1; i++) {
+      setTimeout(() => {
+        this.winBalls.push(this.winNumbers[i])
+      }, (i + 1) * 1000)
+    }
+
+    setTimeout(() => {
+      this.bonus = this.winNumbers[6]
+      this.redo = true
+    }, 7000)
   },
   beforeDestroy() {
 
@@ -46,15 +63,5 @@ export default {
 </script>
 
 <style scoped>
-.ball {
-  display: inline-block;
-  border: 1px solid black;
-  border-radius: 20px;
-  width: 40px;
-  height: 40px;
-  line-height: 40px;
-  font-size: 20px;
-  text-align: center;
-  margin-right: 20px;
-}
+
 </style>
