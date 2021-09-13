20aec3d30627aeba4100924519bce600359e2166
ukdong <lklone20005@gmail.com>
2021-09-13 23:16:51 +0900

    add 로또 번호 추첨

---
+++ 6.로또/LottoGenerator.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>당첨 숫자</div>
+    <div class="ball" v-for="(number, index) in numbers" :key="index">{{number}}</div>
+    <div>보너스!</div>
+    <div class="ball"></div>
+    <button>한 번 더!</button>
+  </div>
+</template>
+
+<script>
+let interval = null
+
+export default {
+  data() {
+    return {
+      numbers: [],
+    }
+  },
+  methods: {
+    genNumbers() {
+      const randomNumber = Math.floor(Math.random() * 45) + 1
+      this.numbers.push(randomNumber)
+    }
+  },
+  mounted() {
+    interval = setInterval(() => {
+      if (this.numbers.length < 6) {
+        this.genNumbers()
+      } else {
+        clearInterval(interval)
+      }
+    }, 1500)
+  },
+  beforeDestroy() {
+
+  },
+  watch: {
+
+  },
+}
+
+</script>
+
+<style scoped>
+.ball {
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-right: 20px;
+}
+</style>
