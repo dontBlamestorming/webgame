a463688442ae2596a1fd69c359ad6a2e9e188838
ukdong <lklone20005@gmail.com>
2021-09-14 15:17:07 +0900

    fix this.$root.$data not working

--- 7.틱택토/main.js
+++ 7.틱택토/main.js
@@ -2,4 +2,5 @@ import Vue from 'vue'
 
 import TicTacToe from "./TicTacToe";
 
-new Vue({ render: createElement => createElement(TicTacToe)}).$mount('#root')
+// new Vue({ render: createElement => createElement(TicTacToe)}).$mount('#root')
+new Vue(TicTacToe).$mount('#root');
