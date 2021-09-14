66cebdf0eb930a1bc16abf69513f5674748fbba5
ukdong <lklone20005@gmail.com>
2021-09-15 01:33:05 +0900

    TicTacToe 프로젝트 Vuex 사용으로 변경준비

---
+++ 7.틱택토Vuex/TdComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <td @click="onClickTd(cellData)">{{cellData}}</td>
+</template>
+
+<script>
+import EventBus from "./EventBus"
+
+export default {
+  props: {
+    cellData: String,
+    rowIndex: Number,
+    cellIndex: Number,
+  },
+  methods: {
+    onClickTd(cellData) {
+      EventBus.$emit('clickTd', this.rowIndex, this.cellIndex, cellData)
+    }
+  }
+}
+</script>
---
+++ 7.틱택토Vuex/TrComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <td-component
+        v-for="(cellData, index) in rowData"
+        :key="index"
+        :row-data="rowData"
+        :row-index="rowIndex"
+        :cell-data="cellData"
+        :cell-index="index">
+    </td-component>
+  </div>
+</template>
+
+<script>
+  import TdComponent from './TdComponent'
+
+  export default {
+    components: {
+      TdComponent,
+    },
+    props: {
+      rowData: Array,
+      rowIndex: Number,
+    },
+    data() {
+      return {
+        parent: 'TrComponent',
+      }
+    }
+  }
+</script>
+
+<style></style>
