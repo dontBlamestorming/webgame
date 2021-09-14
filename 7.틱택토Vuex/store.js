606ae50a4af05a1bc7951344015a73b0309293b2
ukdong <lklone20005@gmail.com>
2021-09-15 01:47:42 +0900

    기초적인 Vuex state 및 mutation 작성

--- 7.틱택토Vuex/store.js
+++ 7.틱택토Vuex/store.js
@@ -1,8 +1,38 @@
 import Vuex from 'vuex'
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    getters: {},
-    actions: {},
+  state: {
+    tableData: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ],
+    turn: 'O',
+    winner: '',
+  },
+  mutations: {
+    // 모두 onClickTd 안에 있었던 로직들
+    SET_WINNER(state, winner) {
+      state.winner = winner
+    },
+    CLICK_CELL(state, {row, cell}) {
+      state.tableData[row][cell] = state.turn
+    },
+    CHANGE_TURN(state) {
+      state.turn = state.turn === 'O' ? 'X' : 'O'
+    },
+    RESET_GAME(state) {
+      state.turn = 'O'
+      state.tableData = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ]
+    },
+    NO_WINNER(state) {
+      state.winner = ''
+    }
+  },
+  getters: {},
+  actions: {},
 })
