bacf20c14a653bce19e334f4d0ce591953636b93
ukdong <lklone20005@gmail.com>
2021-09-16 01:29:49 +0900

    add MineForm for input start game data

---
+++ 8.지뢰찾기/MineForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input type="number" placeholder="세로" :value="row" @change="onChangeRow">
+    <input type="number" placeholder="가로" :value="cell" @change="onChangeCell">
+    <input type="number" placeholder="지회" :value="mine" @change="onChangeMine">
+    <button @click="onClickBtn">START</button>
+  </div>
+</template>
+
+<script>
+import { START_GAME } from "./store";
+
+export default {
+  name: "MineForm",
+  data() {
+    return {
+      row: 10,
+      cell: 10,
+      mine: 20,
+    }
+  },
+  methods: {
+    onChangeRow(e) {
+      this.row = e.target.value
+    },
+    onChangeCell(e) {
+      this.cell = e.target.value
+    },
+    onChangeMine(e) {
+      this.mine = e.target.value
+    },
+    onClickBtn() {
+      const startSweeperData = {
+        row: this.row,
+        cell: this.cell,
+        mine: this.mine
+      }
+
+      this.$store.commit(START_GAME, startSweeperData)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
