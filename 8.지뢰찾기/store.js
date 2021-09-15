55c7cfead38c44228eb3e6b11b144edd9bf52b34
ukdong <lklone20005@gmail.com>
2021-09-16 01:32:10 +0900

    add mine status code && get mine map from plantMine()

--- 8.지뢰찾기/store.js
+++ 8.지뢰찾기/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(vuex)
+Vue.use(Vuex)
 
 export const START_GAME = "START_GAME"
 export const OPEN_CELL = "OPEN_CELL"
@@ -10,6 +10,52 @@ export const FLAG_CELL = 'FLAG_CELL'
 export const QUESTION_CELL = 'QUESTION_CELL'
 export const NORMALIZE_CELL = 'NORMALIZE_CELL'
 
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 다 opened
+}
+
+const plantMine = (row, cell, mine) => {
+  console.log(
+    "Start!",
+    "ROW : " + row,
+    "CELL : " + cell,
+    "COUNT OF MINES : " + mine
+  )
+  const shuffle = []
+  const data = []
+
+  const candidate = Array(row * cell).fill().map((arr, i) => i)
+
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+
+    shuffle.push(chosen)
+  }
+
+  for (let i = 0; i < row; i++) {
+    const rowData = []
+    data.push(rowData)
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL)
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell)
+    const hor = shuffle[k] % cell
+    data[ver][hor] = CODE.MINE
+  }
+
+  console.log("Mine Map", data)
+  return data
+}
 
 export default new Vuex.Store({
   state: {
@@ -23,7 +69,15 @@ export default new Vuex.Store({
     result: '',
   },
   mutations: {
-    [START_GAME](state) {},
+    [START_GAME](state, { row, cell, mine }) {
+      state.data = {
+        row,
+        cell,
+        mine,
+      }
+      state.tableData = plantMine(row, cell, mine)
+      state.timer = 0
+    },
     [OPEN_CELL](state) {},
     [CLICK_MINE](state) {},
     [FLAG_CELL](state) {},
