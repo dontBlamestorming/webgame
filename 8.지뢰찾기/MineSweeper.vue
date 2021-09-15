324b559c25bc95d3ff4c1f24427382ebc8f43d94
ukdong <lklone20005@gmail.com>
2021-09-16 02:44:42 +0900

    add table, td tag style

--- 8.지뢰찾기/MineSweeper.vue
+++ 8.지뢰찾기/MineSweeper.vue
@@ -27,6 +27,14 @@ export default {
 
 </script>
 
-<style scoped>
-
+<style>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid black;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+}
 </style>
