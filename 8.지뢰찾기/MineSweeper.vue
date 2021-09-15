a768dcd2850ec2dad26135aa9be5ca16ad9aeb08
ukdong <lklone20005@gmail.com>
2021-09-16 01:31:09 +0900

    vuex store, child component 연결

--- 8.지뢰찾기/MineSweeper.vue
+++ 8.지뢰찾기/MineSweeper.vue
@@ -1,24 +1,29 @@
 <template>
-
+  <div>
+    <mine-form></mine-form>
+    <div>{{ timer }}</div>
+    <table-component></table-component>
+    <div>{{ result }}</div>
+  </div>
 </template>
 
 <script>
-  import { mpaState } from 'vuex'
-  import store from "./store";
-  import TableComponent from './TableComponent'
+import { mapState } from 'vuex'
+import store from "./store"
+import TableComponent from './TableComponent'
+import MineForm from './MineForm'
 
-  export default {
-    components: {
-      TableComponent,
-    },
-    store,
-    computed: {
-
-    },
-    methods: {
-
-    },
-  }
+export default {
+  components: {
+    TableComponent,
+    MineForm,
+  },
+  store,
+  computed: {
+    ...mapState(['timer', 'result'])
+  },
+  methods: {},
+}
 
 </script>
 
