a52a9de99b2ab099dc0fd2d2493f53826c0bf545
ukdong <lklone20005@gmail.com>
2021-09-17 22:57:53 +0900

    vue router basic configuration

---
+++ vue-router/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+
+import MineSweeper from "./MineSweeper";
+
+// new Vue({ render: createElement => createElement(TicTacToe)}).$mount('#root')
+new Vue(MineSweeper).$mount('#root');
